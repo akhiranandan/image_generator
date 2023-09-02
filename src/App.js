62b06271d09b682fd5e288e842ee3116ef952a5e
Akhira Nandan <akhiranandanp89548@gmail.com>
2023-09-02 18:49:19 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <ImageGenerator/>
+    </div>
   );
 }
 
